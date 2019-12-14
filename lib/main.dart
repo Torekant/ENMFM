12af5214b0dd22499191346d1ef5e65a9ddbdf42
Tony <blehramos@gmail.com>
2019-12-13 19:21:51 -0600

    optimización y comentarios en SplashScreen //// 2.0.1 ////////// -Splash Screen con comentarios y optimizada.
    //////////// Seguir con Limpieza y optimización de código y comentarios /////////////////////////

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'values.dart';
 import 'dart:async';
 import 'screens.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,15 +43,6 @@ class MyApp extends StatelessWidget {
 class SplashScreen extends StatefulWidget {
   SplashScreen({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -70,6 +60,8 @@ class _SplashScreen extends State<SplashScreen> {
     super.initState();
     values = new Values();
     hue  = new Hues();
+
+    //Inicio de timer para esperar a pasar a la pantalla principal
     Timer(Duration(seconds: values.defaultTimer), () {
       Navigator.pushReplacement(
           context,
@@ -78,11 +70,6 @@ class _SplashScreen extends State<SplashScreen> {
           )
       );
     });
-    final FirebaseMessaging _messaging = FirebaseMessaging();
-    _messaging.getToken().then((token){
-      print(token);
-    });
-
   }
 
   @override
@@ -94,73 +81,23 @@ class _SplashScreen extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
 
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double _screenWidth = MediaQuery.of(context).size.width; //Ancho de pantalla
 
-    double _screenWidth = MediaQuery.of(context).size.width; //lee el ancho de dispositivo
-    //double _screenHeight = MediaQuery.of(context).size.height; //lee el largo del dispositivo
-    double _symmetricPadding = values.defaultSymmetricPadding; //padding lateral de la pantalla
-
-    _symmetricPadding =  _screenWidth * values.widthPaddingUnit; //Función que nos permite hacer un padding responsivo a cualquier resolución en ancho
-
-    return OrientationBuilder(
-      builder: (context, orientation){
-        return orientation == Orientation.portrait
-            ?
-        Scaffold(
-            appBar: AppBar(
-              backgroundColor: hue.carmesi,
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: hue.carmesi,
+        ),
+        backgroundColor: hue.background,
+        body: Center(
+          child: Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.symmetric(horizontal: _screenWidth / 10),
+            child: Image.asset(
+              values.logoColored,
+              fit: BoxFit.contain,
             ),
-            backgroundColor: hue.background,
-            body: new Container(
-              child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: _symmetricPadding / 1.5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                        height: values.bigSizedBoxStandardHeight,
-                        child: Image.asset(
-                          values.logoColored,
-                          fit: BoxFit.contain,
-                        ),
-                      ),
-                    ],
-                  )
-              ),
-            )
+          ),
         )
-            :
-        Scaffold(
-            appBar: AppBar(
-              backgroundColor: hue.carmesi,
-            ),
-            backgroundColor: hue.background,
-            body: new Container(
-              child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: _symmetricPadding),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(
-                        height: values.bigSizedBoxStandardHeight,
-                        child: Image.asset(
-                          values.logoColored,
-                          fit: BoxFit.contain,
-                        ),
-                      ),
-                    ],
-                  )
-              ),
-            )
-        );
-      },
     );
   }
 }
